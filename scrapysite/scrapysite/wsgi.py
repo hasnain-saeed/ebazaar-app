e860791aede624654f29715b951e81ede8f544f0
Hasnain Saeed <hasnain.saeed@arbisoft.com>
2020-10-22 18:14:34 +0500

    Final Commit

--- scrapysite/scrapysite/wsgi.py
+++ scrapysite/scrapysite/wsgi.py
@@ -3,6 +3,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapysite.scrapysite.settings')
 
 application = get_wsgi_application()
